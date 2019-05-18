e768df43763bccafe5a5fb567b08461d5d1206e6
Bryan Arendt <barendt@covermymeds.com>
2019-05-18 13:57:11 -0400

    start adding bootstrap

---
+++ src/components/Nav.vue
@@ -0,0 +1,8 @@
+<template>
+  <header>
+    <div id="nav" class="navbar navbar-dark bg-dark">
+      <router-link class="navbar-brand" to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </header>
+</template>
